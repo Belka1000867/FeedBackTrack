cf91ed21e097a8e3b156bc6b94cc9a7eb5448894
Aleksandr Namanyuk <aleksandr.namanyuk@gmail.com>
2016-11-26 17:41:46 +0200

    initial project version with androidannotation

---
+++ app/src/main/java/com/home/bel/feedbacktrack/MainActivity.java
@@ -0,0 +1,19 @@
+package com.home.bel.feedbacktrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
+}
