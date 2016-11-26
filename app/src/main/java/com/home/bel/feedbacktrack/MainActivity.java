19a17efb433203bf57074fb0b743ccc176d72c18
Aleksandr Namanyuk <aleksandr.namanyuk@gmail.com>
2016-11-27 00:46:22 +0200

    Implemented QR scanner

--- app/src/main/java/com/home/bel/feedbacktrack/MainActivity.java
+++ app/src/main/java/com/home/bel/feedbacktrack/MainActivity.java
@@ -1,14 +1,25 @@
 package com.home.bel.feedbacktrack;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.home.bel.feedbacktrack.ui.MainFragment_;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.home.bel.feedbacktrack.ui.MainFragment;
+import com.home.bel.feedbacktrack.ui.StartFragment;
+import com.home.bel.feedbacktrack.ui.StartFragment_;
 
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StartFragment.LoginButtonHandler {
+
+    private static final String TAG = "Debug_MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,36 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager()
                 .beginTransaction()
                 .addToBackStack(null)
-                .add(R.id.mainFrameLayout, new MainFragment_(), "FRAGMENT_START_TAG")
+                .add(R.id.mainFrameLayout, new StartFragment_(), "FRAGMENT_START_TAG")
                 .commit();
 
     }
 
 
+    @Override
+    public void signIn() {
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.mainFrameLayout, new MainFragment(), "FRAGMENT_MAIN_TAG")
+                .commit();
+
+    }
+
+    @Override
+    public void signUp() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "There is some result returned for an Activity!");
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag("FRAGMENT_MAIN_TAG");
+        if (fragment != null) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
--- app/src/main/java/com/home/bel/feedbacktrack/ui/MainFragment.java
+++ app/src/main/java/com/home/bel/feedbacktrack/ui/MainFragment.java
@@ -1,13 +1,74 @@
 package com.home.bel.feedbacktrack.ui;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.home.bel.feedbacktrack.R;
 
 import org.androidannotations.annotations.EFragment;
 
-@EFragment(R.layout.fragment_start)
-public class MainFragment extends Fragment {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+@EFragment(R.layout.fragment_main)
+public class MainFragment extends Fragment{
+
+    private static final String TAG = "Debug_MainFragment";
+    
+    IntentIntegrator intentIntegrator;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        intentIntegrator = new IntentIntegrator(getActivity());
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+//        intentIntegrator.setPrompt("Scan");
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setBeepEnabled(true);
+        intentIntegrator.setBarcodeImageEnabled(false);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG, "Result is ready in the activity! Let's show it!");
+
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        String scanData = (scanningResult != null) ? scanningResult.getContents() : "";
+
+        if (scanData == null || scanData.isEmpty()) {
+            Toast.makeText(getActivity(), "Scan complete, no data", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getActivity(), scanData, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
 
 
 
